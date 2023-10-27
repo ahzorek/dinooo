593e31c19d9892214117136ef53d61928bcdb7f0
Andre <andrezorek@gmail.com>
2023-10-27 13:25:50 -0300

    you idiot

--- routes/_middleware.ts
+++ routes/_middleware.ts
@@ -1,7 +1,7 @@
 import { MiddlewareHandlerContext } from "$fresh/server.ts";
 
 export async function handler(req: Request, ctx: MiddlewareHandlerContext) {
-  // console.log(ctx);
+  console.log(ctx);
   console.log(req);
   const resp = await ctx.next();
   return resp;
