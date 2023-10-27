f75278baa74eedb2533ac56533d3d58dbaf593af
Andre <andrezorek@gmail.com>
2023-10-27 13:09:07 -0300

    trying to grab me some sweet ctx. failing. heh

---
+++ islands/PrintRequest.tsx
@@ -0,0 +1,14 @@
+// // import type { Signal } from "@preact/signals";
+// import { ComponentChildren } from "preact";
+
+// interface Props {
+//   children: ComponentChildren;
+// }
+
+// export default function PrintRequest({ children }: Props) {
+//   return (
+//     <div class="flex gap-8 py-6">
+//       {children}
+//     </div>
+//   );
+// }
--- routes/index.tsx
+++ routes/index.tsx
@@ -1,23 +1,25 @@
 import { useSignal } from "@preact/signals";
 import Counter from "../islands/Counter.tsx";
+import IconBrandDeno from "https://deno.land/x/tabler_icons_tsx@0.0.5/tsx/brand-deno.tsx";
 
 export default function Home() {
   const count = useSignal(3);
+
   return (
     <div class="px-4 py-8 mx-auto bg-[#86efac]">
       <div class="max-w-screen-md mx-auto flex flex-col items-center justify-center">
-        <img
-          class="my-6"
-          src="/logo.svg"
-          width="128"
-          height="128"
-          alt="the Fresh logo: a sliced lemon dripping with juice"
-        />
-        <h1 class="text-4xl font-bold">Welcome to Fresh</h1>
-        <p class="my-4">
-          Try updating this message in the
-          <code class="mx-2">./routes/index.tsx</code> file, and refresh.
-        </p>
+        <IconBrandDeno class="w-20 h-20" />
+        <h1 class="text-4xl">Hey!</h1>
+        <h2 class="text-2xl">been trying to meet you</h2>
+        <h3>Hey</h3>
+        <p>Must be a devil between us</p>
+        <p>Or whores in my head</p>
+        <p>Whores at the door</p>
+        <p>Whore in my bed</p>
+        <p>But hey</p>
+        <p>Where have you been?</p>
+        <p>If you go, I will surely die</p>
+
         <Counter count={count} />
       </div>
     </div>
