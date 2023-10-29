f98927b512b84719a5d8eebc86c3382608a40d62
Andre <andrezorek@gmail.com>
2023-10-29 00:55:14 -0300

    its pretty late isnt it?

--- routes/index.tsx
+++ routes/index.tsx
@@ -4,14 +4,15 @@ import IconBrandDeno from "https://deno.land/x/tabler_icons_tsx@0.0.5/tsx/brand-
 
 export default function Home(props: PageProps) {
   const count = useSignal(3);
-  console.log(props);
-  console.log(Deno.env.get("GEOIP"));
+  console.log(props.state);
+  // console.log(Deno.env.get("GEOIP"));
   
   return (
     <div class="px-4 py-8 mx-auto bg-[#86efac]">
       <div class="max-w-screen-md mx-auto flex flex-col items-center justify-center">
         <IconBrandDeno class="w-40 h-40" />
         <Counter count={count} />
+        {props.state.ip}
       </div>
     </div>
   );
